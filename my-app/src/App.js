682af887d4ea1631e691c64a4201319bf0e972bf
while-kaique <123902283+while-kaique@users.noreply.github.com>
2023-10-27 18:18:00 -0300

    Adicionando arquivos
    Primeiro dia

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {  
+  const name = 'kaique'
+  return (
+    <div className="App">
+      <h1>Eae cara</h1>
+      <p>Seu nome é {name}, né?</p>
+    </div>
+  );
+}
+
+export default App;
