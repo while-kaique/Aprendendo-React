fc7c3fe228038ee5ecb5f0603a64d5e373e96bab
while-kaique <123902283+while-kaique@users.noreply.github.com>
2023-10-31 02:24:39 -0300

    Entendendo sobre conexão entre parentes
    Também aprendi umas coisinhas

--- my-app/src/components/Form.jsx
+++ my-app/src/components/Form.jsx
@@ -1,16 +1,31 @@
+import { useState } from 'react'
+
 function Form () {
 
     function cadastrarUsuario(e) {
+        console.log(name)
         console.log('Cadastrou o usuário')
+        console.log(`O usuário é "${name}" e sua senha é: "${password}"`)
         e.preventDefault()
     }
+
+    const [name, setName] = useState('SEM NOME')
+
+    const [password, setPassword] = useState('INDEFINIDA')
 
     return (
         <>
             <h1>Meu cadastro</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite seu nome"/>
+                    <label htmlFor="username">Seu nome: </label>
+                    <input type="text" name='name' id="username" placeholder="Digite seu nome"
+                    onChange={(e) => {setName(e.target.value)}}/>
+                </div>
+                <div>
+                    <label htmlFor="password">Sua senha: </label>
+                    <input type="password" id="password" placeholder="Digite sua senha"
+                    onChange={(e)=>{setPassword(e.target.value)}}/>
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar" />
